1182495099febc4653919816392ba71e93cf9507
seracettin <11200832+seracettin@users.noreply.github.com>
2019-11-05 11:27:11 +0300

    added

---
+++ BasicStringOperations/main.py
@@ -0,0 +1,23 @@
+#first occured
+astring = "Hello world!"
+print(astring.index("o"))
+
+print(astring.count("l"))
+
+#[3,6=7-1]
+print(astring[3:7])
+
+#[start:stop:step]
+#0,2,4,
+print(astring[0:7:2])
+
+#reverse string
+print(astring[::-1])
+
+print(astring.upper())
+print(astring.lower())
+
+print(astring.startswith("Hello"))
+print(astring.endswith("asdfasdfasdf"))
+
+
