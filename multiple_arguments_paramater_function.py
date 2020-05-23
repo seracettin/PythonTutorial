c99e0015cd944229ae0da7e2a7ac3039dad409bc
seracettin <11200832+seracettin@users.noreply.github.com>
2020-05-23 14:21:06 +0300

    updated

--- multiple_arguments_paramater_function.py
+++ multiple_arguments_paramater_function.py
@@ -8,6 +8,13 @@ def fooOneStar(first, second, third, *one):
 def barTwoStar(first, second, third, **param):
      print(param["action"])
 
+def get_follow(self, **parameters):
+    self.get_only_page(**parameters)
+
+def get_only_page(self, **parameters):
+    self.to_comment = parameters["to_comment"]
+    self.webdriver.get(parameters["share_link"])
 
 fooOneStar(1,2,3,4,5)
 barTwoStar(1, 2, 3, action = "sum", number = "first")
+get_follow(share_link= "https://www.instagram.com/test", to_comment= "mest")
